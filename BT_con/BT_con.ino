a2ea9c136bcb56cf40b6a26a9fd8933c61782319
kuzukawakonayuki <konayuki0530@outlook.jp>
2019-11-16 16:20:08 +0900

    Update BT_con.ino

--- BT_con/BT_con.ino
+++ BT_con/BT_con.ino
@@ -1,40 +1,49 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
 
-SoftwareSerial BT(10, 11);
-Servo myservo;
-int servo_r = 90;
-char a;
+#define tx 11
+#define rx 10
+#define ctl 9
 
-void setup()
-{
-  BT.begin(9600);
-  myservo.attach(9);
+SoftwareSerial btSerial(rx,tx);
+Servo myservo;
+
+char mode = '0';
+int servo_r = 90;
+
+void setup() {
+  btSerial.begin(9600);
+  myservo.attach(ctl);
   myservo.write(servo_r);
+
 }
 
-void loop()
-{
-  if (BT.available()) {
-    a = (BT.read());
-    if (a == '2') {
-      BT.println("r+");
-      if (servo_r <= 180){
-      servo_r = servo_r + 10;
-      }
-      myservo.write(servo_r);
-    }
-    if (a == '1') {
-      BT.println("r-");
-      if (servo_r >= 0){
-      servo_r = servo_r - 10;
-      }
-      myservo.write(servo_r);
-    }
-    if (a == '0') {
-      BT.println("r_reset");
+void loop() {
+
+  if(btSerial.available()){
+    mode = btSerial.read();
+
+    switch(mode){
+    case '0':
+      btSerial.println("REST\n");
       servo_r = 90;
       myservo.write(servo_r);
+      break;
+    case '1':
+      btSerial.println("R+\n");
+      if (servo_r > 0) {
+        servo_r = servo_r - 10;
+      }
+      myservo.write(servo_r);
+      break;
+    case '2':
+      btSerial.println("R-\n");
+      if (servo_r < 180) {
+        servo_r = servo_r + 10;
+      }
+      myservo.write(servo_r);
+      break;
     }
-    }
+  }
+
 }
