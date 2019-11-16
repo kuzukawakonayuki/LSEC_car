bf52b844d7e3c2935ec4219f314e2f15cfb00372
kuzukawakonayuki <konayuki0530@outlook.jp>
2019-11-16 16:56:27 +0900

    Update BT_con.ino

--- BT_con/BT_con.ino
+++ BT_con/BT_con.ino
@@ -4,6 +4,8 @@
 #define tx 11
 #define rx 10
 #define ctl 9
+#define ledr 8
+#define ledl 7
 
 SoftwareSerial btSerial(rx,tx);
 Servo myservo;
@@ -14,6 +16,10 @@ int servo_r = 90;
 void setup() {
   btSerial.begin(9600);
   myservo.attach(ctl);
+  pinMode(ledr, OUTPUT);
+  digitalWrite(ledr, LOW);
+  pinMode(ledl, OUTPUT);
+  digitalWrite(ledl, LOW);
   myservo.write(servo_r);
 
 }
@@ -28,20 +34,26 @@ void loop() {
       btSerial.println("REST\n");
       servo_r = 90;
       myservo.write(servo_r);
+      digitalWrite(ledr, LOW);
+      digitalWrite(ledl, LOW);
       break;
     case '1':
-      btSerial.println("R+\n");
+      btSerial.println("R-\n");
       if (servo_r > 0) {
         servo_r = servo_r - 10;
       }
       myservo.write(servo_r);
+      digitalWrite(ledr, HIGH);
+      digitalWrite(ledl, LOW);
       break;
     case '2':
-      btSerial.println("R-\n");
+      btSerial.println("R+\n");
       if (servo_r < 180) {
         servo_r = servo_r + 10;
       }
       myservo.write(servo_r);
+      digitalWrite(ledr, LOW);
+      digitalWrite(ledl, HIGH);
       break;
     }
   }
